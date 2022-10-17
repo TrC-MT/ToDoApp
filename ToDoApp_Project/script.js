c6f534ab720cbaf07074684f741fc79d28536730
Travis Cook <travis.cook5583@stu.mtec.edu>
2022-10-17 09:34:34 -0600

    Deleting completed items in a better way.

--- ToDoApp_Project/script.js
+++ ToDoApp_Project/script.js
@@ -213,8 +213,8 @@ it puts them into a new object, coverted back to the old object. */
   function CID(){ //Completed Items Delete
     for(var i = 0; i < currentList.items.length; i++){
       console.log(i)
-      console.log(currentList.items[i].item)
-      if(currentList.items[i].item.includes(`<span class="completedItemcheck">&#10003</span>   `)){
+      console.log(currentList.items[i].completed)
+      if(currentList.items[i].completed == true){
         for(var j = i; j < currentList.items.length; j++){
           if(currentList.items[j+1] != null){
             currentList.items[j] = currentList.items[j+1]
